3591bd18a04fc8e83256a7ea8b6ae2157e4c5ea5
Joao-Quinta <qjoao.filipe@gmail.com>
2022-03-18 15:25:39 +0100

    asd

--- random_python_code/amanda.py
+++ random_python_code/amanda.py
@@ -8,24 +8,29 @@ def extended_euclid_gcd(a, b):
             result[2] is y -> v
         """
         s = 0
-        old_s = 1 # u
+        old_s = 1  # u
         t = 1
-        old_t = 0 # v
+        old_t = 0  # v
         r = b
         old_r = a
 
         while r != 0:
+            a = old_r
+            # print("-> ", old_r)
             quotient = old_r // r  # In Python, // operator performs integer or floored division
             # print("oldr = r * quotient -> ", old_r, r, quotient)
             # This is a pythonic way to swap numbers
             # See the same part in C++ implementation below to know more
-            old_r, r = r, old_r - quotient * r # c'est bon
-            print(old_s, s, old_t, t)
+            old_r, r = r, old_r - quotient * r  # c'est bon
+
             old_s, s = s, old_s - quotient * s
             old_t, t = t, old_t - quotient * t
+
+            # print(quotient, " = ", old_r, r, " u -> {", old_s, s, "}", " v -> {", old_t, t, "}")
+            print(r, " = ", a, " * ", old_s, " + ", old_r, " * ", old_t, " -> ", a * old_s + old_r * old_t)
         return [old_r, old_s, old_t]
     else:
         return extended_euclid_gcd(b, a)
 
 
-print(extended_euclid_gcd(17, 33))
+print(extended_euclid_gcd(164, 72))
